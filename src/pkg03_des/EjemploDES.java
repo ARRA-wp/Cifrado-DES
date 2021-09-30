ce7638452c1740159edaff36cf47ab56c1630955
ARRA-wp <72839394+ARRA-wp@users.noreply.github.com>
2021-09-30 15:44:26 -0500

    Cifrado DES
    Cifrado DES

---
+++ src/pkg03_des/EjemploDES.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg03_des;
+
+/**
+ *
+ * @author Rafael
+ */
+import java.security.MessageDigest;
+import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.swing.JOptionPane;
+import org.apache.commons.codec.binary.Base64;
+
+
+
+
+public class EjemploDES {
+
+   //Utilizamos las librerias de crypto y security, 
+    //de java para atender los cifrados simétricos y asimétricos
+
+    
+
+
+    
+    
+    public static void main(String[] args) throws Exception {
+       
+       
+        Cara1 p = new Cara1();
+        p.setVisible(true);
+        
+    }
+    
+    
+    
+    
+    
+    
+}
